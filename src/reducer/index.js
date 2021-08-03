208cf65cc18fd668ace19ce1c8e356147f8e2174
Dreezy305 <bankoleidris@gmail.com>
2021-08-03 20:04:14 +0000

    first commit

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,2 +1,10 @@
 import petCounter from "./petCounter";
 import petFavorite from "./petFavourite";
+import { combineReducers } from "redux";
+
+const reducers = combineReducers({
+  petCounter,
+  petFavorite,
+});
+
+export default reducers;
